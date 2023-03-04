ad277c2234fa35daa14db786087e9ffea9daf177
Anthony Lo <87.anthonylo@gmail.com>
2023-03-04 08:09:50 -0800

    test

--- src/layouts/SectionContainer.jsx
+++ src/layouts/SectionContainer.jsx
@@ -6,7 +6,7 @@ function SectionContainer({ children, bgColor }) {
       test
     */}
       <section
-        className={`h-screen w-screen flex flex-col justify-center snap-start ${bgColor}`}
+        className={`h-screen w-screen flex flex-col justify-center snap-center ${bgColor}`}
       >
         {children}
       </section>
