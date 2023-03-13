2167e02b713e75bdc6fc6303ba65f8aeb19fb89f
Anthony Lo <87.anthonylo@gmail.com>
2023-03-13 12:45:30 -0700

    update nav bar underline to follow section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,15 @@
 import { useContext } from 'react';
 import { appContext } from '../providers/appProvider';
 
-function NavLink({ children, refType }) {
-  const { scrollIntoView } = useContext(appContext);
+function NavLink({ id, children, refType }) {
+  const { scrollIntoView, selSection } = useContext(appContext);
 
   return (
     <li className='p-3'>
       <button
-        className='hover:underline hover:underline-offset-4'
+        className={`hover:underline hover:underline-offset-4 ${
+          id === selSection ? `underline underline-offset-4` : ``
+        }`}
         onClick={() => scrollIntoView(refType)}
       >
         {children}
@@ -24,10 +26,18 @@ function NavBar() {
     <nav className={`fixed inset-x-0 top-0 w-full font-bold ${color}`}>
       <div className='max-w-7xl m-auto flex flex-row justify-center lg:justify-end'>
         <ul className='flex flex-row'>
-          <NavLink refType={aboutRef}>01. About</NavLink>
-          <NavLink refType={expRef}>02. Experience</NavLink>
-          <NavLink refType={projectRef}>03. Projects</NavLink>
-          <NavLink refType={contactRef}>04. Contact</NavLink>
+          <NavLink id={0} refType={aboutRef}>
+            01. About
+          </NavLink>
+          <NavLink id={1} refType={expRef}>
+            02. Experience
+          </NavLink>
+          <NavLink id={2} refType={projectRef}>
+            03. Projects
+          </NavLink>
+          <NavLink id={3} refType={contactRef}>
+            04. Contact
+          </NavLink>
         </ul>
       </div>
     </nav>
